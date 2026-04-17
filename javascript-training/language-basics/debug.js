74f640f170c37fc80fc391113ed283736accedc5
Priti Pawar <priti.rahul.pawar@gmail.com>
2026-04-17 16:13:39 +0530

    Assignment 1

--- javascript-training/language-basics/debug.js
+++ javascript-training/language-basics/debug.js
@@ -1,16 +1,36 @@
-console.log("This is line 1");
-console.log("This is line 2");
-console.log("This is line 3");
-console.log("This is line 4");
-sumOfNumbers(5,10);
-console.log("This is line 5");
-console.log("This is line 6");
-console.log("This is line 7");
-console.log("This is line 8");
-console.log("This is line 9");
-console.log("This is line 10");
 
-//program to add two numbers
+console.log("Executing line 1");
+console.log("Executing line 2");
+console.log("Executing line 3");
+console.log("Executing line 4");
+sumOfNumbers(5,10);
+console.log("Executing line 5");
+console.log("Executing line 6");
+console.log("Executing line 7");
+console.log("Executing line 8");
+console.log("Executing line 9");
+console.log("Executing line 10");
+
+// Debug the program step by step
+
+// 1. adding the breakpoints (the line where you want to stop the execution of program)
+// 2. run the program in debug mode
+// 3. wait for the following options to appear
+
+        // continue => continues the execution of program until the next break point is hit or the program ends
+        // stop => stops the execution of the program
+        // restart => restart the execution of the program from the beginning
+
+        // step over => executes the current line and move to the next line
+        // step into => go inside the current line and check the background code
+        // step out => exits the current function and returns to main program
+
+
+
+
+
+
+//Logic to get the sum of two numbers
 function sumOfNumbers(a,b){
         c=a+b;
         console.log(c);
--- javascript-training/language-basics/hello-world.js
+++ javascript-training/language-basics/hello-world.js
@@ -1,5 +1,13 @@
 console.log("Hello World!...");
 
+//Print statement benefits
+// 1. Eduction and learning
+// 2. Debugging and Trobleshooting
+// 3. Monitoring and logging
+// 4. Testing and validation
+// 5. Communication and collaboration
+
+// Other ways to print in Javascript (Debugging and Logging)
 console.log("----------------console.info-----------------------");
 console.info("This statement is used to provide informational message at run time.");
 
@@ -7,6 +15,6 @@ console.log("----------------console.warn-----------------------");
 console.warn("This statement is used to provide warning message at run time.");
 
 console.log("----------------console.error----------------------");
-console.warn("This statement is used to provide error message at run time.");
+console.error("This statement is used to provide error message at run time.");
 
 
